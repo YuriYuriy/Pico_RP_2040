4f7ed60311bfb1429a99aec07392a7d6d82d667d
YuriYuriy <yuri.golovin.75@gmail.com>
2025-09-09 21:43:22 +0500

    Add files via upload

---
+++ Pico_RP_2040_PetitFatFs/main.h
@@ -0,0 +1,21 @@
+//----------------------------------------------------------------------------------------------------
+// (C) 2025 YuriYuri (Yuriy Golovin)
+//----------------------------------------------------------------------------------------------------
+#ifndef _MAIN
+#define _MAIN
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+
+#include "spi_config.h"
+#include "sd_card.h"
+#include "pff.h"
+
+#endif
